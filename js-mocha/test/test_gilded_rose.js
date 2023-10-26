4326abf7ac8f3a663c2e14555ad1836080d5aa56
Gabriel Vejar <g.vejarperez@gmail.com>
2023-10-26 21:28:50 +1300

    Generate a test suite with the legacy code

--- js-mocha/test/test_gilded_rose.js
+++ js-mocha/test/test_gilded_rose.js
@@ -8,4 +8,39 @@ describe("Gilded Rose", function() {
     expect(items[0].name).to.equal("fixme");
   });
 
+  // Generate a test suite with the legacy code 
+  it("Generate a test suite with the legacy code ", function() {
+
+    const names = [
+      'Aged Brie',
+      'Backstage passes to a TAFKAL80ETC concert',
+      'Sulfuras, Hand of Ragnaros',
+      'normal item'
+    ];
+    const [minQuality, maxQuality] = [-1, 51];
+    const [minSellIn, maxSellIn] = [-1, 12];
+
+    const testsCases = [];
+
+    names.forEach(name => {
+      for (let incomeQuality = minQuality; incomeQuality <= maxQuality; incomeQuality++) {
+        for (let incomeSellIn = minSellIn; incomeSellIn <= maxSellIn; incomeSellIn++) {
+          const gildedRose = new Shop([ new Item(name, incomeSellIn, incomeQuality) ]);
+          const items = gildedRose.updateQuality();
+          const {sellIn, quality} = items[0];
+
+          testsCases.push({
+            name,
+            incomeSellIn,
+            incomeQuality,
+            sellIn, 
+            quality
+          })
+        }
+      }
+    });
+
+    console.log(JSON.stringify(testsCases, null, 2));
+  });
+
 });
