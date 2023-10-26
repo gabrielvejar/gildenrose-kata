1bb0c0ca8d92fc0fb627953b9a0592eeeb0fad74
Gabriel Vejar <g.vejarperez@gmail.com>
2023-10-26 22:02:38 +1300

    Create individual methods for brie, passes, sulfuras, and normal items

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -39,47 +39,17 @@ const updatePasses = (item) => {
 }
 
 const updateSulfuras = (item) => {
-  if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-    if (item.quality > 0) {
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.quality = item.quality - 1;
-      }
-    }
-  } else {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-      if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.sellIn < 11) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-        if (item.sellIn < 6) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
-    }
+  // sulfuras doesn't updates quality nor sellIn
+}
+
+const updateNormalItem = (item) => {
+  if (item.quality > 0) {
+    item.quality = item.quality - 1;
   }
-  if (item.name != 'Sulfuras, Hand of Ragnaros') {
-    item.sellIn = item.sellIn - 1;
-  }
+  item.sellIn = item.sellIn - 1;
   if (item.sellIn < 0) {
-    if (item.name != 'Aged Brie') {
-      if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality = item.quality - 1;
-          }
-        }
-      } else {
-        item.quality = item.quality - item.quality;
-      }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-      }
+    if (item.quality > 0) {
+      item.quality = item.quality - 1;
     }
   }
 }
@@ -89,6 +59,12 @@ class Shop {
     this.items = items;
   }
   updateQuality() {
+    const updateMethods = {
+      BRIE: updateBrie,
+      PASSES: updatePasses,
+      SULFURAS: updateSulfuras,
+      default: updateNormalItem
+    }
     this.items.forEach(function (item) {
 
       updateSulfuras(item)
